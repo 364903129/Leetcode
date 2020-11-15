b18fa70faea05796db926c538db29ff30ecb19fc
Haofan Wang <sassycabbagebit@gmail.com>
2020-11-14 20:04:43 -0800

    add bfs

--- Leetcode17.java
+++ Leetcode17.java
@@ -1,4 +1,4 @@
-// Letter Combinations of a Phone Number - DFS
+// -----------------------Letter Combinations of a Phone Number - DFS ----------------------
 class Solution {
     HashMap<String,String> number = new HashMap<String, String>(){{
         put("2", "abc");
@@ -32,6 +32,41 @@ class Solution {
     public List<String> letterCombinations(String digits) {
         if(digits.length() != 0)
             DFS("", digits);           
+        return result; 
+    }
+}
+
+//--------------------- Letter Combinations of a Phone Number - BFS -------------------------
+class Solution {
+    HashMap<Character,String> number = new HashMap<Character, String>(){{
+        put('2', "abc");
+        put('3', "def");
+        put('4', "ghi");
+        put('5', "jkl");
+        put('6', "mno");
+        put('7', "pqrs");
+        put('8', "tuv");
+        put('9', "wxyz");
+    }};
+    
+    List<String> result = new ArrayList<String>();
+    public List<String> letterCombinations(String digits) {
+        if(digits == null || digits.length() == 0)
+            return result;
+        result.add("");
+        //1. convert the digit to char array, so we can loop each digit
+        for(char ch : digits.toCharArray()){
+            List<String> temp = new ArrayList<>();
+            //2. loop each comb in result
+            for(String combination : result){
+                String letters = number.get(ch);
+                //3. add every digits letter
+                for(int i = 0; i < letters.length(); i++){
+                    temp.add(combination + letters.charAt(i));
+                }
+            }
+            result = temp;
+        }
         return result; 
     }
 }
