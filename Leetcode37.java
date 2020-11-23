f5898a70949d3578d684f3d74d6e7f451182b6b4
Haofan Wang <sassycabbagebit@gmail.com>
2020-11-23 15:02:44 -0800

    Create Leetcode37.java

---
+++ Leetcode37.java
@@ -0,0 +1,48 @@
+// ------------------------- sudoku solver ----------------------------
+/* Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+A sudoku solution must satisfy all of the following rules:
+
+Each of the digits 1-9 must occur exactly once in each row.
+Each of the digits 1-9 must occur exactly once in each column.
+Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+The '.' character indicates empty cells. */
+
+// Time complexity: O(9^m) where m is the number of blank space.
+// Space complexity: O(n^2) where n is the length of the board.
+
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    
+    private boolean solve(char[][] board){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                if(board[i][j] == '.'){
+                    for(char k = '1';k <='9';k++){
+                        if(vaild(board, i, j, k)){
+                            board[i][j] = k;
+                            if(solve(board)){
+                                return true;
+                            }else{
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    private boolean vaild(char[][] board, int width, int height, char num){
+        for(int i = 0; i < 9; i++){
+            if(board[i][height] == num) return false;
+            if(board[width][i] == num) return false;
+            if(board[3 * (width / 3) + i / 3][3 * (height / 3) + i % 3] == num) return false;
+        }
+        return true;
+    }
+}
